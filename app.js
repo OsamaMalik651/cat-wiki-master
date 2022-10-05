d0f8678a771db187cdab62eb0aff2555b20d5143
OsamaMalik651 <osama.malik013@gmai.com>
2022-10-04 19:28:12 -0600

    fixed bugs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require("express");
+// const axios = require("axios")
+const axios = require("axios")
 const path = require('path');
 const bodyParser = require("body-parser");
 var cors = require('cors');
@@ -8,10 +10,37 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.get("/", async (req, res) => {
+    //Get breeds from API
+    const response = await axios.get('https://api.thecatapi.com/v1/breeds', {
+        headers: {
+            'x-api-key': process.env.API_KEY
+        },
+    }).then(response => {
+        return response;
+    }).catch(err => {
+        console.log(err);
+    });
 
-app.get("/", (req, res) => {
-    res.send("Server connected")
+    //Function to get randomly selected Cat Breeds
+    function getMultipleRandom(arr, num) {
+        const shuffled = [...arr].sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, num);
+    }
+
+    const results = getMultipleRandom(response.data, 10);
+    //Filter the selected keys only
+    const filteredData = [...results].map((obj) => {
+        let object = Object.fromEntries(
+            Object.entries(obj).filter(
+                ([key]) => key.match("id") || key.match("name") || key.match("description") || key.match("image")
+            )
+        );
+        return object;
+    });
+    res.send(filteredData)
 })
+
 
 //Set up port
 const port = process.env.PORT || 5000;
